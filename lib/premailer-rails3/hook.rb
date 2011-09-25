35d7bd9b22aab6a06cd732db4b636ec6a45cfca0
Harri Kauhanen <swit@humaneq.com>
2011-09-25 15:28:44 +0300

    text/html part no longer twice

--- lib/premailer-rails3/hook.rb
+++ lib/premailer-rails3/hook.rb
@@ -11,19 +11,27 @@ module PremailerRails
         message.body = nil
       end
 
+
       if html_body
+        text_part = message.text_part
+        message.body = nil # otherwise we get two text/html parts into the mail
+
         premailer = Premailer.new(html_body)
         charset   = message.charset
-
+        
         message.html_part do
           content_type "text/html; charset=#{charset}"
           body premailer.to_inline_css
         end
 
-        message.text_part do
-          content_type "text/plain; charset=#{charset}"
-          body premailer.to_plain_text
-        end unless message.text_part
+        if text_part
+          message.text_part = text_part
+        else
+          message.text_part do
+            content_type "text/plain; charset=#{charset}"
+            body premailer.to_plain_text
+          end
+        end
       end
     end
   end
